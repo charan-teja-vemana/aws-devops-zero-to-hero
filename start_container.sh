4000073762b49c6b36bc8130acc143753d34a1ff
charan-teja-vemana <94907972+charan-teja-vemana@users.noreply.github.com>
2025-03-02 23:13:58 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,7 @@
 
+
+# Pull the Docker image from Docker Hub
+docker pull charan852/simple-python-flask-app
+# Run the Docker image as a container
+docker run -d -p 5000:5000 charan852/simple-python-flask-app
+
